1d40203fa23698335f89d74ba3a131233b33a30b
Seppe De Langhe <seppedelanghe17@gmail.com>
2021-01-12 23:46:25 +0100

    remove support for old import

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="SimpleSQLConnector",
-    version="0.0.1",
+    version="0.0.2",
     author="fastskyz",
     author_email="seppedelanghe17@gmail.com",
     description="A simple SQL Connector",
