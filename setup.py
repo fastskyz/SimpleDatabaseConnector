fb1130cb05d5fc3efc9734f51a0e72270193b526
Seppe De Langhe <seppedelanghe17@gmail.com>
2021-01-13 12:05:15 +0100

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="SimpleDatabaseConnector",
-    version="0.0.3",
+    version="0.0.4",
     author="fastskyz",
     author_email="seppedelanghe17@gmail.com",
     description="Simple Database Connectors",
